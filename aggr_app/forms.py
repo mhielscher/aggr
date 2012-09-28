855658ce24d9ef4588da3d412949edfbca0a11fe
Matthew Hielscher <mhielscher@gmail.com>
2012-09-27 17:15:15 -0700

    Added forms for new_feed and new_aggr.

---
+++ aggr_app/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from aggr_app.models import Feed
+
+feed_choices = tuple([(feed.id, feed.name) for feed in Feed.objects.all()])
+
+class NewFeedForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    url = forms.URLField(max_length=400, required=True)
+
+class NewAggrForm(forms.Form):
+    """Variable form."""
+    name = forms.CharField(max_length=100)
+    filter_count = forms.CharField(widget=forms.HiddenInput())
+    
+    def __init__(self, *args, **kwargs):
+        """Initialize the form with a variable number of filter fields.
+        
+        At least one of the following arguments required:
+        filter_count: The number of filter fields to include.
+        filters: A tuple of 2-tuples matching feed id to filter string.
+        
+        If filter_count is less than the number of filters, filter_count is ignored.
+        Otherwise, extra fields are filled in with defaults.
+        """
+        filters = kwargs.pop('filters', tuple())
+        filter_count = int(kwargs.pop('filter_count', 1))
+        super(NewAggrForm, self).__init__(*args, **kwargs)
+        if filter_count < len(filters):
+            filter_count = len(filters)
+        self.fields['filter_count'].initial = filter_count
+        for i in range(filter_count):
+            self.fields['feed%d' % i] = forms.ChoiceField(choices=feed_choices)
+            self.fields['filter%d' % i] = forms.CharField(max_length=400)
+        for i in range(len(filters)):
+            self.fields['feed%d' % i].initial = filters[i][0]
+            self.fields['filter%d' % i].initial = filters[i][1]
+            
+    """
+    def __init__(self, *args, **kwargs):
+        ""\"Initialize the form with a variable number of filter fields.
+        
+        filters: Required. A tuple of 2-tuples matching feed id to filter string.
+        ""\"
+        filters = kwargs.pop('filters', ((0,''),))
+        super(NewAggrForm, self).__init__(*args, **kwargs)
+        self.fields['filter_count'].initial = len(filters)
+        for i in range(num_filters):
+            self.fields['feed%d' % i] = forms.ChoiceField(choices=feed_choices)
+            self.fields['feed%d' % i].initial = filters[i][0]
+            self.fields['filter%d' % i] = forms.CharField(max_length=400)
+            self.fields['filter%d' % i].initial = filters[i][1]
+    """
--- aggr_app/views.py
+++ aggr_app/views.py
@@ -1,5 +1,6 @@
 from aggr_app.models import Feed, FilteredFeed, Aggregate
 from aggr_app.feeds import AggregateFeed
+from aggr_app.forms import NewFeedForm, NewAggrForm
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response, get_object_or_404
@@ -36,40 +37,45 @@ def new_feed(request):
     POST: attempt to create the new feed.
     """
     if request.method == 'GET':
-        return render(request, 'aggr_app/feed_new.html')
+        form = NewFeedForm()
+        return render(
+            request,
+            'aggr_app/feed_new.html',
+            {'form': form}
+        )
     
     elif request.method == 'POST':
-        try:
-            name = request.POST['name']
-        except KeyError:
-            return render(
-                request,
-                'aggr_app/feed_new.html',
-                {'error_message': 'Name is a required field.'}
-            )
+        form = NewFeedForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            url = form.cleaned_data['url']
+            
+            if Feed.objects.filter(url=url).exists():
+                return render(
+                    request,
+                    'aggr_app/feed_new.html',
+                    {
+                        'error_message': 'That feed already exists.',
+                        'form': form
+                    }
+                )
         
-        try:
-            url = request.POST['url']
-        except KeyError:
+            # Finally create the new Feed.
+            feed = Feed(name=name, url=url)
+            feed.save()
+            return HttpResponseRedirect(
+                reverse('aggr_app.views.feed_detail',
+                args=(feed.id,))
+            )
+        else:
             return render(
                 request,
                 'aggr_app/feed_new.html',
-                {'error_message': 'URL is a required field.'}
+                {
+                    'form': form
+                }
             )
             
-        if Feed.objects.filter(url=url).exists():
-            return render(
-                request,
-                'aggr_app/feed_new.html',
-                {'error_message': 'That feed already exists.'}
-            )
-        
-        # Finally create the new Feed.
-        feed = Feed(name=name, url=url)
-        feed.save()
-        return HttpResponseRedirect(
-            reverse('aggr_app.views.feed_detail',
-            args=(feed.id,)))
 
 def delete_feed(request, feed_id):
     """Confirm (GET) and delete (POST) an Feed.
@@ -138,57 +144,76 @@ def new_aggr(request, aggr_id=None):
     """
     logger.debug("aggr_id=%s" % aggr_id)
     if request.method == 'GET':
-        feed_list = Feed.objects.all().order_by("name")
         if aggr_id:
             aggr = Aggregate.objects.get(pk=aggr_id)
+            form = NewAggrForm(initial={'name': aggr.name}, filters=aggr.feed_tuple())
         else:
             aggr = None
+            form = NewAggrForm()
+        
+        # Neat trick to turn ['feed0', 'filter0', 'feed1', 'filter1', ...]
+        # into [('feed0', 'filter0'), ('feed1', 'filter1'), ...]
+        filter_fields_iter = list(form)[2:].__iter__()
+        filter_fields = zip(filter_fields_iter, filter_fields_iter)
+        
         return render(
             request,
             'aggr_app/aggr_new.html',
             {
-                'feed_list': feed_list,
-                'aggr': aggr
+                'aggr': aggr,
+                'form': form,
+                'filter_fields': filter_fields
             }
         )
     
     elif request.method == 'POST':
-        try:
-            logger.debug("trying")
+        logger.debug("trying")
+        filter_count = int(request.POST.get('filter_count'))
+        form = NewAggrForm(request.POST, filter_count=filter_count)
+        if form.is_valid():
+            logger.debug("Form is valid.")
             if aggr_id:
                 logger.debug("Modifying existing Aggr id=%s" % aggr_id)
                 aggr = Aggregate.objects.get(pk=aggr_id)
-                # Delete all existing filters and remake them later.
-                for feed in aggr.feeds.all():
-                    feed.delete()
+                aggr.name = form.cleaned_data['name']
             else:
                 logger.debug("Creating new Aggr")
-                aggr = Aggregate(name=request.POST['name'])
+                aggr = Aggregate(name=form.cleaned_data['name'])
             
-            # Some stupid magic to iterate through feedN and filterN
-            num_filters = max([int(f[len('feed')]) for f in request.POST.keys() if f.find('feed')==0])+1
             feed_filters = []
-            for (feed, feed_filter) in (('feed%d'%i, 'filter%d'%i) for i in range(num_filters)):
-                feed_obj = Feed.objects.get(pk=request.POST[feed])
+            logger.debug("filter_count=%d" % (filter_count,))
+            for (feed, feed_filter) in (('feed%d'%i, 'filter%d'%i) for i in range(filter_count)):
+                feed_obj = Feed.objects.get(pk=form.cleaned_data[feed])
                 logger.debug(feed_obj)
-                filtered_feed = FilteredFeed(feed=feed_obj, re_filter=request.POST[feed_filter])
+                filtered_feed = FilteredFeed(feed=feed_obj, re_filter=form.cleaned_data[feed_filter])
                 filtered_feed.save()
                 feed_filters.append(filtered_feed)
-        except KeyError as e:
-            logger.debug("error: %s", e.message)
-            return render(request, 'aggr_app/aggr_new.html', {'error_message': 'Something is missing.'})
         else:
-            # Pre-save the model before we can use the ManyToManyField.
-            if not aggr_id:
-                aggr.save()
-            logger.debug(feed_filters)
-            aggr.feeds = feed_filters
-            aggr.save()
-            logger.debug("success")
-            return HttpResponseRedirect(
-                reverse('aggr_app.views.aggr_detail',
-                args=(aggr.id,))
+            logger.debug("Form NOT valid.")
+            logger.debug(form)
+            aggr = None
+            filter_fields_iter = list(form)[2:].__iter__()
+            filter_fields = zip(filter_fields_iter, filter_fields_iter)
+            return render(
+                request,
+                'aggr_app/aggr_new.html',
+                {
+                    'aggr': aggr,
+                    'form': form,
+                    'filter_fields': filter_fields
+                }
             )
+        # Pre-save the model before we can use the ManyToManyField.
+        if not aggr_id:
+            aggr.save()
+        logger.debug(feed_filters)
+        aggr.feeds = feed_filters
+        aggr.save()
+        logger.debug("success")
+        return HttpResponseRedirect(
+            reverse('aggr_app.views.aggr_detail',
+            args=(aggr.id,))
+        )
             
 def delete_aggr(request, aggr_id):
     """Confirm (GET) and delete (POST) an Aggregate.
